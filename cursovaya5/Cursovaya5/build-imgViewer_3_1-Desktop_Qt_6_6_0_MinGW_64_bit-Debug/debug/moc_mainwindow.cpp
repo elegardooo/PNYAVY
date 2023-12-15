8e2b843ebdcc60de0b2458d2adb10c5b84a7742b
elegardooo <112643027+elegardooo@users.noreply.github.com>
2023-12-15 23:05:53 +0300

    Add files via upload

---
+++ cursovaya5/Cursovaya5/build-imgViewer_3_1-Desktop_Qt_6_6_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,237 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../ImageViewer/mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.6.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "on_action_triggered",
+    "",
+    "on_action_3_triggered",
+    "on_listWidget_itemDoubleClicked",
+    "QListWidgetItem*",
+    "item",
+    "on_action_4_triggered",
+    "on_action_5_triggered",
+    "on_action_6_triggered",
+    "on_action_7_triggered",
+    "on_action_10_triggered",
+    "on_action_11_triggered",
+    "on_action_2_triggered"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[28];
+    char stringdata0[11];
+    char stringdata1[20];
+    char stringdata2[1];
+    char stringdata3[22];
+    char stringdata4[32];
+    char stringdata5[17];
+    char stringdata6[5];
+    char stringdata7[22];
+    char stringdata8[22];
+    char stringdata9[22];
+    char stringdata10[22];
+    char stringdata11[23];
+    char stringdata12[23];
+    char stringdata13[22];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 19),  // "on_action_triggered"
+        QT_MOC_LITERAL(31, 0),  // ""
+        QT_MOC_LITERAL(32, 21),  // "on_action_3_triggered"
+        QT_MOC_LITERAL(54, 31),  // "on_listWidget_itemDoubleClicked"
+        QT_MOC_LITERAL(86, 16),  // "QListWidgetItem*"
+        QT_MOC_LITERAL(103, 4),  // "item"
+        QT_MOC_LITERAL(108, 21),  // "on_action_4_triggered"
+        QT_MOC_LITERAL(130, 21),  // "on_action_5_triggered"
+        QT_MOC_LITERAL(152, 21),  // "on_action_6_triggered"
+        QT_MOC_LITERAL(174, 21),  // "on_action_7_triggered"
+        QT_MOC_LITERAL(196, 22),  // "on_action_10_triggered"
+        QT_MOC_LITERAL(219, 22),  // "on_action_11_triggered"
+        QT_MOC_LITERAL(242, 21)   // "on_action_2_triggered"
+    },
+    "MainWindow",
+    "on_action_triggered",
+    "",
+    "on_action_3_triggered",
+    "on_listWidget_itemDoubleClicked",
+    "QListWidgetItem*",
+    "item",
+    "on_action_4_triggered",
+    "on_action_5_triggered",
+    "on_action_6_triggered",
+    "on_action_7_triggered",
+    "on_action_10_triggered",
+    "on_action_11_triggered",
+    "on_action_2_triggered"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      10,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   74,    2, 0x08,    1 /* Private */,
+       3,    0,   75,    2, 0x08,    2 /* Private */,
+       4,    1,   76,    2, 0x08,    3 /* Private */,
+       7,    0,   79,    2, 0x08,    5 /* Private */,
+       8,    0,   80,    2, 0x08,    6 /* Private */,
+       9,    0,   81,    2, 0x08,    7 /* Private */,
+      10,    0,   82,    2, 0x08,    8 /* Private */,
+      11,    0,   83,    2, 0x08,    9 /* Private */,
+      12,    0,   84,    2, 0x08,   10 /* Private */,
+      13,    0,   85,    2, 0x08,   11 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 5,    6,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_action_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_action_3_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_listWidget_itemDoubleClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>,
+        // method 'on_action_4_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_action_5_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_action_6_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_action_7_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_action_10_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_action_11_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_action_2_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_action_triggered(); break;
+        case 1: _t->on_action_3_triggered(); break;
+        case 2: _t->on_listWidget_itemDoubleClicked((*reinterpret_cast< std::add_pointer_t<QListWidgetItem*>>(_a[1]))); break;
+        case 3: _t->on_action_4_triggered(); break;
+        case 4: _t->on_action_5_triggered(); break;
+        case 5: _t->on_action_6_triggered(); break;
+        case 6: _t->on_action_7_triggered(); break;
+        case 7: _t->on_action_10_triggered(); break;
+        case 8: _t->on_action_11_triggered(); break;
+        case 9: _t->on_action_2_triggered(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 10)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 10;
+    }
+    return _id;
+}
+QT_WARNING_POP
